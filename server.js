d4bb396e782375145de70255c3b732d1184668fa
ljyockey <linyockey@gmail.com>
2017-07-01 22:21:15 -0700

    setup CORS

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get('/api/*', (req, res) => {
 	res.json({ok: true});
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
 module.exports = {app};
