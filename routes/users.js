eda31ee2577adace368729677a6e62c930b08581
ljyockey <linyockey@gmail.com>
2017-07-04 20:02:00 -0700

    user routes

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,46 @@ router.get('/', (req, res) => User.findAll()
   	user.apiRepr())}))
 );
 
+router.get('/:id', (req, res) => User.findById(req.params.id)
+  .then(user => res.json(user.apiRepr())));
+
+
+//POST request
+router.post('/', (req, res) => {
+  const requiredFields = ['email', 'name', 'password'];
+  requiredFields.forEach(item => {
+    if(!(item in req.body)) {
+      const missingContentMessage = `missing ${item} in req.body`;
+      return res.status(400).send(missingContentMessage);
+    }
+  });
+  //checks for existing email
+  return User.findOne({where: {email: req.body.email}})
+    .then(userCheck => {
+      if(userCheck !== null) {
+        const emailExistsMessage = 'email address already taken';
+        return res.status(400).send(emailExistsMessage);
+      } else {
+        const newUser = {
+          email: req.body.email,
+          password: req.body.password,
+          name: req.body.name,
+          fbId: req.body.fbId
+        };
+        return User.create(newUser)
+          .then(user =>
+            res.status(201).json(user.apiRepr()))
+          .catch(err => 
+            res.status(500).json({message: 'internal server error'}));
+      }
+    });
+});
+
+//DELETE request
+router.delete('/:id', (req, res) => {
+  return User.destroy({where: {id: req.params.id}})
+    .then(user => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'internal server error'}));
+});
+
 module.exports = router;
--- test/test-users-endpoints.js
+++ test/test-users-endpoints.js
@@ -11,27 +11,82 @@ chai.use(chaiHttp);
 
 describe('Users API resource', function() {
 
-	beforeEach(function() {
-		console.log('seeding data');
-		return seedTestData();
-	});
+  beforeEach(function() {
+    console.log('seeding data');
+    return seedTestData();
+  });
 
-	afterEach(function() {
-		console.log('dropping tables');
-		return dropTables();
-	});
+  afterEach(function() {
+    console.log('dropping tables');
+    return dropTables();
+  });
 
-	describe('GET endpoints: users', function() {
+  describe('GET endpoints: users', function() {
 
-		it('should return all users', function() {
-			return chai.request(app)
-			.get('/users')
-			.then(function(res) {
-			res.should.have.status(200);
-			res.should.be.a('object');
-		});
-		});
+    it('should return all users', function() {
+      return chai.request(app)
+        .get('/users')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.a('object');
+        });
+    });
 
-	});
+    it('should return user by ID', function() {
+      let user;
+      return User.findOne()
+        .then(function(_user) {
+          user = _user;
+          return chai.request(app)
+            .get(`/users/${user.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(200);
+          res.body.id.should.equal(user.id);
+        });
+    });
+
+  });
+
+  describe('POST requests: users', function() {
+
+    it('should add new user to DB', function() {
+      let newUser;
+      return chai.request(app)
+        .post('/users').send(buildUser())
+        .then(function(res) {
+          newUser = res.body;
+          res.should.have.status(201);
+          return User.findOne({where: {id: newUser.id}});
+        })
+        .then(function(user) {
+          user.id.should.equal(newUser.id);
+          user.name.should.equal(newUser.name);
+          user.email.should.equal(newUser.email);
+        });
+    });
+
+  });
+
+  describe('DELETE requests: users', function() {
+
+    it('should delete user', function() {
+      let user;
+      return User.findOne()
+        .then(function(_user) {
+          user = _user;
+          return chai.request(app)
+            .delete(`/users/${user.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return User.findOne({where: {id: user.id}});
+        })
+        .then(function(res) {
+          should.not.exist(res);
+        });
+    });
+
+  });
 
 });
